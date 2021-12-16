9a60fa8ab9947c072d37d6bae12cba214855e32b
suthyscott <sutherlandscott21@gmail.com>
2021-12-16 16:48:58 -0700

    added more advanced filtering

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const rollbar = new Rollbar({
 let students = []
 
 const app = express()
-// app.use(express.json())
+app.use(express.json())
 
 
 
@@ -21,15 +21,24 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully.')
 })
 
-app.post('/api/student', (req, res)=> {
+app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('Student added successfully', {author: "Scott", type: 'manual'})
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
 
 
